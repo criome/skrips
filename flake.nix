1b4d1620d1f2e103289e5f04f2a59fcc72487216
li <li@maisiliym.criome.me>
2025-05-23 16:26:50 -0400

    autoFormat(nix)

--- flake.nix
+++ flake.nix
@@ -1,37 +1,49 @@
 {
   description = "Skrips";
 
-  outputs = { self }: {
-    SobUyrld = {
-      modz = [ "pkgs" "pkdjz" ];
+  outputs =
+    { self }:
+    {
+      SobUyrld = {
+        modz = [
+          "pkgs"
+          "pkdjz"
+        ];
 
-      lamdy = { runCommand, mksh, sd }:
-        let
-          inherit (builtins) readFile readDir mapAttrs;
+        lamdy =
+          {
+            runCommand,
+            mksh,
+            sd,
+          }:
+          let
+            inherit (builtins) readFile readDir mapAttrs;
 
-          mkSkrip = name: srcPath:
-            let
-              mksheBang = "#!" + mksh + "/bin/mksh";
-            in
-            runCommand name { inherit srcPath; } ''
-              mkdir --parents $out/bin/
-              for skrip in $srcPath/*
-              do
-              export neim=$(basename $skrip)
-              cp ''$skrip $out/bin/''${neim%.sh}
-              done
-              ${sd}/bin/sd --string-mode '#!/usr/bin/env mksh' '${mksheBang}' $out/bin/*
-              chmod 755 $out/bin/*
-            '';
+            mkSkrip =
+              name: srcPath:
+              let
+                mksheBang = "#!" + mksh + "/bin/mksh";
+              in
+              runCommand name { inherit srcPath; } ''
+                mkdir --parents $out/bin/
+                for skrip in $srcPath/*
+                do
+                export neim=$(basename $skrip)
+                cp ''$skrip $out/bin/''${neim%.sh}
+                done
+                ${sd}/bin/sd --string-mode '#!/usr/bin/env mksh' '${mksheBang}' $out/bin/*
+                chmod 755 $out/bin/*
+              '';
 
-          mkTopDirEntry = name: fileType:
-            assert (fileType == "directory");
-            mkSkrip name (self + "/${name}");
+            mkTopDirEntry =
+              name: fileType:
+              assert (fileType == "directory");
+              mkSkrip name (self + "/${name}");
 
-          topDir = removeAttrs (readDir self) [ "flake.nix" ];
+            topDir = removeAttrs (readDir self) [ "flake.nix" ];
 
-        in
-        mapAttrs mkTopDirEntry topDir;
+          in
+          mapAttrs mkTopDirEntry topDir;
+      };
     };
-  };
 }
